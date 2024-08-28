6fc35d713ac2e42f8587f91b62d2aab0cab87d9e
madhubalasenthil <160823415+madhubalasenthil@users.noreply.github.com>
2024-08-28 15:40:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let tran = document.getElementById("tran");
+let show =  document.getElementById("show");
+let cancel = document.getElementById("cancel");
+
+show.addEventListener("click",()=>{
+    tran.style.display="block";
+    
+    
+    
+});
+cancel.addEventListener("click",()=>{
+    tran.style.display="none";
+});
